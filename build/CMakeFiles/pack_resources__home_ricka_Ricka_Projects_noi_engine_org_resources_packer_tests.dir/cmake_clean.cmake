a705a43d7bd93aec593a7811d4707e2a3e698595
RickaPrincy <rckprincy@gmail.com>
2026-08-15 18:41:26 +0300

    chore: delete build

--- build/CMakeFiles/pack_resources__home_ricka_Ricka_Projects_noi_engine_org_resources_packer_tests.dir/cmake_clean.cmake
+++
@@ -1,8 +0,0 @@
-file(REMOVE_RECURSE
-  "CMakeFiles/pack_resources__home_ricka_Ricka_Projects_noi_engine_org_resources_packer_tests"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang )
-  include(CMakeFiles/pack_resources__home_ricka_Ricka_Projects_noi_engine_org_resources_packer_tests.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
--- build/CMakeFiles/pack_resources__home_ricka_Ricka_Projects_noi_engine_org_resources_packer_tests.dir/compiler_depend.ts
+++
@@ -1,2 +0,0 @@
-# CMAKE generated file: DO NOT EDIT!
-# Timestamp file for custom commands dependencies management for pack_resources__home_ricka_Ricka_Projects_noi_engine_org_resources_packer_tests.
